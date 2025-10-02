aa754c6126c488d6f15a56e9a75e39c98c410baa
Jesper Sandtangen <166068406+jsandtangen@users.noreply.github.com>
2025-10-02 10:32:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import mean_var_std
 from unittest import main
+# import pprint as pprint
 
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+# If desirable you could use the pprint func:
+# pprint(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+# main(module='test_module', exit=False)
